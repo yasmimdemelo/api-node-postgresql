ef3a3a1816834797a979eaa18e7d9e72f0b7ba3a
Yasmim <yasmimdmf95@gmail.com>
2023-02-03 15:55:25 -0300

    feat: install and import body-parser, import database to app.js file and add method post to insert datas in the database

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const app = express();
 
+//Import body-parser para analisar corpos de solitações
+const bodyParser = require('body-parser');
+
+//Importe db 
+const pool = require('./config/db');
+
 //import modulo path
 const path = require('path');
 
@@ -12,13 +18,34 @@ app.set('view engine', 'ejs');
 //Config statics files
 app.use(express.static(__dirname + '/public'));
 
-//Config database file
-// app.use(express.config(__dirname + '/config'));
+//Configure o analisador de corpo para analisar corpos de solicitação JSON
+app.use(bodyParser.json());
 
 //usamos app.get() para definirmos uma rota e a resposta correspondente
 app.get('/', (req, res) => {
     res.render('index', {name: 'My Express App'});
 });
+
+app.post('/task', async (req, res) => {
+    try {
+        //Extract task_name from request body
+        const { task_name } = req.body;
+
+        //Insert the new taskinto the database
+        const client = await pool.connect();
+        const result = await client.query('INSERT INTO tasks (task_name) VALUES($1) RETURNING task_name', [task_name]);
+        const id = result.rows[0].id;
+        client.release(); //release libera a conexao do cliente acima com o database
+
+        //Send the newly created task in the response
+        res.status(201).send({ task_name });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error inserting task into database");
+    }
+});
+
 
 //app listen método para iniciar o servidor e escutar em uma porta específica
 app.listen(3000, () => {
