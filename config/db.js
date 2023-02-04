ec2e3b8d3cda03b775f6d812ebbb838c8ad67697
Yasmim <yasmimdmf95@gmail.com>
2023-02-04 16:45:14 -0300

    fix: change database name

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     host: 'localhost',
     port: '5432',
-    database: 'PERN',
+    database: 'postgres',
     user: 'myusername',
     password: 'mypassword'
 });
